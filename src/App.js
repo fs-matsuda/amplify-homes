25f4716e279bbb67baefda892d7a65a9d976630b
eiji.matsuda <matsuda@foresight.co.jp>
2022-06-04 15:25:32 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { NewHomes, NavBar, MarketingFooter } from './ui-components'
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+return (
+
+<div className="App">
+  <NavBar width={"100vw"}/>
+  <NewHomes isPaginated itemsPerPage={3}/>
+  <MarketingFooter width={"100vw"}/>
+</div>
+
+);
 }
 
 export default App;
